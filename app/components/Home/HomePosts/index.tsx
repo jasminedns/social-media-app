3fe06631899c9ac23a06422e75adc21e1bd8091a
Jasminedns <jasminedanese@icloud.com>
2025-10-10 17:03:59 +0200

    fixed homepage

--- app/components/Home/HomePosts/index.tsx
+++ app/components/Home/HomePosts/index.tsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 
 const HomePosts = ({posts}:{posts:HomePostsType}) => {
     return (
-        <div className="flex flex-wrap">
+        <div className="md:flex md:flex-wrap">
             { posts && posts.map(({id, slug, title, users, image}) =>
-            <div key={id} className={`${image ? "flex flex-col md:flex-row justify-between md:items-center" : ""} m-3 rounded-2xl border-1 w-[47%]`}>
+            <div key={id} className={`${image ? "flex flex-col md:flex-row justify-between md:items-center" : ""} m-3 rounded-2xl border-1 md:w-[47%]`}>
                 <Link href={`/${slug}`} className="block m-4 p-3">
                     <p>By {users.username}</p>
                     <h2 className="font-bold ml-3">{title}</h2>
